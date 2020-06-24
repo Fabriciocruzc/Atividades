fa4b3a3500c51023a5f22e18a921d31af25587d0
Fabriciocruzc <32718377+Fabriciocruzc@users.noreply.github.com>
2020-06-24 18:18:30 -0300

    Update Diary.cpp

--- Diary/src/Diary.cpp
+++ Diary/src/Diary.cpp
@@ -1,12 +1,25 @@
+#include <iostream>
 #include "../include/Diary.h"
 
-Diary::Diary(const std::string& filename)
+Diary::Diary(const std::string& filename): messages_capacity(10), messages(nullptr), messages_size(0)
 {
+    messages = new Message[messages_capacity];
 }
 
 void Diary::add(const std::string& message)
 {
     // adicionar mensagem no array de mensagens
+    if (messages_capacity == messages_size) {
+        std:: cout << "Nao suporta mais mensagens" << std::endl;
+        
+    }else {
+        Message m;
+        m.content = message;
+        messages[messages_size] = m;
+        messages_size++;
+    }
+
+    
 }
 
 void Diary::write()
